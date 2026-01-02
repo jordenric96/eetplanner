128b87ace8bca26000c4f982eb05e761ba0680c3
jordenric96 <jordenricour96@gmail.com>
2026-01-02 16:21:23 +0100

    Update recepten_w1&2.js

--- februari/recepten_w1&2.js
+++ februari/recepten_w1&2.js
@@ -1,1 +1,25 @@
-test
+var shoppingData = {
+    ricour: {
+        v: ["Nog geen vlees ingepland"],
+        g: ["Nog geen groenten ingepland"],
+        k: ["Nog geen voorraad ingepland"]
+    },
+    hemmeryckx: {
+        v: ["Nog geen vlees ingepland"],
+        g: ["Nog geen groenten ingepland"],
+        k: ["Nog geen voorraad ingepland"]
+    }
+};
+
+var recipeData = {
+    recipes: [
+        { 
+            m: "Bestand wordt momenteel ingevuld", 
+            kcal: 0, p: 0, t: 0, 
+            ing: ["Geduld"], 
+            steps: ["Binnenkort verschijnen hier de recepten voor februari."], 
+            kid: "Nog even wachten!" 
+        }
+    ]
+};
+
