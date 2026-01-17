829499cbedc256798090ffb39846e7b4a7382f78
jordenric96 <jordenricour96@gmail.com>
2026-01-17 10:32:54 +0100

    Update recepten_w3&4.js

--- januari/recepten_w3&4.js
+++ januari/recepten_w3&4.js
@@ -1,152 +1,155 @@
-/* DATA VOOR JANUARI WEEK 3 & 4 - UPDATE MET VERSPREIDE SPAGHETTI & KIP */
+/* DATA VOOR JANUARI WEEK 3 & 4 - HIGH PROTEIN & VEGGIE UPDATE */
 
 var shoppingData = {
     ricour: {
         // VOORRAAD: 1kg Kipblokjes, 2kg Gehakt, Rijst, DV Spinazie, Tomatenblokjes
-        v: ["Kippenchipolata (500g) - €6.99", "Tonijn in eigen nat (3 blikjes) - €4.50", "DV Koolvisfilet (800g) - €5.99", "Eieren (6 stuks) - €1.95"],
-        g: ["Paprika mix (3st) - €1.99", "Prei (bussel) - €1.49", "Wortelen (2kg zak) - €1.99", "Aardappelen (2.5kg) - €3.99", "Rode Kool met appel (pot) - €1.45", "Ui (net) - €1.25", "Mais (3 pack) - €2.25", "Rode bonen (Kidney) - €0.99", "Kikkererwten (blik 400g) - €0.89", "Zoete Aardappel (1kg) - €2.50"],
-        k: ["Pasta (500g) - €0.95", "Tomatenpuree (klein) - €0.65", "Room (250ml) - €1.15", "Geraspte kaas (200g) - €2.19", "Ketjap Manis - €1.89", "Kokosmelk (blik) - €1.45"]
+        // AANGEPAST: Meer groenten (Bloemkool/Broccoli) en eiwitrijke zuivel (Hüttenkäse/Kwark)
+        v: ["Kippenchipolata (500g) - €6.99", "Tonijn in eigen nat (3 blikjes) - €4.50", "DV Koolvisfilet (800g) - €5.99", "Eieren (12 stuks) - €3.89"],
+        g: ["Paprika mix (3st) - €1.99", "Prei (bussel) - €1.49", "Wortelen (2kg zak) - €1.99", "Bloemkool (vers of DV) - €2.49", "Broccoli (vers of DV) - €1.89", "Ui (net) - €1.25", "Mais (3 pack) - €2.25", "Rode bonen (Kidney) - €0.99", "Kikkererwten (blik 400g) - €0.89", "Zoete Aardappel (1kg) - €2.50"],
+        k: ["Volkoren Pasta (indien mogelijk) - €1.15", "Tomatenpuree - €0.65", "Hüttenkäse/Cottage Cheese (200g) - €1.19", "Geraspte kaas 30+ - €2.19", "Ketjap Manis - €1.89", "Kokosmelk light - €1.45"]
     },
     hemmeryckx: {
         v: ["Kippenchipolata (350g) - €4.99", "Tonijn (2 blikjes) - €3.00", "DV Koolvis (400g) - €3.50", "Eieren (6 stuks) - €1.95"],
-        g: ["Paprika (2st) - €1.35", "Prei (2 stuks) - €1.00", "Wortelen (1kg) - €1.15", "Aardappelen - €2.00", "Rode Kool (pot) - €1.45", "Zoete Aardappel (2st) - €1.50", "Kikkererwten (klein blik) - €0.70"],
-        k: ["Pasta - €0.70", "Room (klein) - €0.85", "Kaas - €1.50", "Kokosmelk (klein) - €1.15"]
+        g: ["Paprika (2st) - €1.35", "Prei (2 stuks) - €1.00", "Wortelen (1kg) - €1.15", "Bloemkool - €1.50", "Broccoli - €1.20", "Zoete Aardappel - €1.50"],
+        k: ["Pasta - €0.70", "Hüttenkäse - €1.19", "Kaas - €1.50", "Kokosmelk - €1.15"]
     }
 };
 
 var recipeData = {
     recipes: [
-        /* SOEPEN - LUNCH */
+        /* SOEPEN - PROTEÏNE BOOST */
         { 
-            id: "j_w3_s1", m: "LUNCH - SOEP: Tomaat-Paprika", kcal: 110, p: 3, t: 25,
-            ing: ["VOORRAAD: Tomatenblokjes", "2 Paprika's", "1 Ui", "Gedroogde Oregano", "Bouillon"],
+            id: "j_w3_s1", m: "LUNCH: Linzen-Tomatensoep", kcal: 140, p: 8, t: 25,
+            ing: ["VOORRAAD: Tomatenblokjes", "1 blik Linzen (extra kopen of uit voorraad)", "Paprika", "Bouillon"],
             steps: [
-                "1. Fruit de ui en grove stukken paprika aan in olijfolie.",
-                "2. Voeg de tomatenblokjes en groentebouillon toe.",
-                "3. Laat koken tot zacht en mix glad. Kruid stevig met **oregano en tijm**."
+                "1. Fruit paprika en ui. Voeg tomatenblokjes en bouillon toe.",
+                "2. Voeg een uitgelekt blik linzen toe (dit zorgt voor binding én proteïne).",
+                "3. Mix glad. De linzen maken de soep romig zonder room."
             ],
-            kid: "Lekkere rode soep."
+            kid: "Rode soep die goed vult."
         },
         { 
-            id: "j_w3_s2", m: "LUNCH - SOEP: Wortel-Gember", kcal: 95, p: 2, t: 25,
-            ing: ["500g Wortelen", "1 Aardappel", "Gemberpoeder"],
+            id: "j_w3_s2", m: "LUNCH: Broccolisoep met Zalm (optie)", kcal: 110, p: 6, t: 20,
+            ing: ["Broccoli", "1 Ui", "Bouillon", "Restje hüttenkäse"],
             steps: [
-                "1. Kook de wortelen en aardappel in kippenbouillon.",
-                "2. Mix glad. Voeg een snufje **gember** toe voor de weerstand."
+                "1. Kook de broccoli gaar in bouillon.",
+                "2. Mix glad met een lepel hüttenkäse voor romigheid.",
+                "3. (Optioneel: voeg snippers gerookte zalm toe als topping)."
             ],
-            kid: "Oranje soep."
+            kid: "Hulk soep."
         },
 
         /* WEEK 3 */
         { 
-            id: "j_w3_1", m: "Ma 19/01: Kip 'Cacciatore' (Jagerskip)", kcal: 420, p: 30, t: 25,
-            ing: ["VOORRAAD: 350g Kipblokjes", "VOORRAAD: Tomatenblokjes", "VOORRAAD: Rijst", "1 Paprika", "Gedroogde Tijm"], 
+            id: "j_w3_1", m: "Ma 19/01: Kip 'Cacciatore' (Extra Groenten)", kcal: 450, p: 35, t: 25,
+            ing: ["VOORRAAD: 350g Kipblokjes", "VOORRAAD: Tomatenblokjes", "VOORRAAD: Rijst (kleine portie)", "2 Paprika's", "Ui"], 
             steps: [
-                "1. Bak de kipblokjes bruin. Haal even uit de pan.",
-                "2. Bak ui en paprikablokjes glazig. Voeg de tomatenblokjes en 1 el **tijm** toe.",
-                "3. Doe de kip terug en laat 10 min sudderen tot de saus indikt.",
-                "4. Serveer met gekookte rijst."
+                "1. Bak de kipblokjes bruin. Haal eruit.",
+                "2. Bak ui en VEEL paprika (2 stuks). Hoe meer groenten, hoe beter.",
+                "3. Voeg de tomatenblokjes, tijm en oregano toe. Stoof de kip gaar in de saus.",
+                "4. Serveer met een kleinere portie rijst, de saus vult genoeg."
             ], 
-            kid: "Kip in een zachte rode saus." 
+            kid: "Kip in rode saus met zoete paprika." 
         },
         { 
-            id: "j_w3_2", m: "Di 20/01: Koolvis met Wortelstoemp", kcal: 390, p: 25, t: 30,
-            ing: ["DV Koolvisfilet", "Aardappelen", "Wortelen", "Melk/Boter"], 
+            id: "j_w3_2", m: "Di 20/01: Koolvis met Bloemkoolstoemp", kcal: 380, p: 30, t: 30,
+            ing: ["DV Koolvisfilet", "1/2 Bloemkool", "500g Wortelen", "Weinig aardappel (bindmiddel)"], 
             steps: [
-                "1. Kook aardappelen en wortelen samen gaar. Stamp tot puree met melk/boter en nootmuskaat.",
-                "2. Dep de vis droog en bak in boter goudbruin. Kruid met peper, zout en eventueel wat citroensap.",
-                "3. Serveer de vis bovenop de stamppot."
+                "1. Kook de bloemkool, wortelen en een klein beetje aardappel gaar.",
+                "2. Stamp tot puree. Door de bloemkool heb je veel volume maar weinig koolhydraten.",
+                "3. Bak de koolvis in olijfolie/boter en serveer op de berg groentepuree.",
+                "4. Kruid goed af met nootmuskaat en peper."
             ], 
-            kid: "Oranje puree met zachte witte vis." 
+            kid: "Een berg oranje/witte puree." 
         },
         { 
-            id: "j_w3_3", m: "Wo 21/01: Spaghetti Bolognese (Deel 1)", kcal: 650, p: 35, t: 45,
-            ing: ["VOORRAAD: 1kg Gehakt (de helft vd voorraad)", "Pasta", "Spaghettigroenten (ui/wortel/prei uit voorraad)", "Italiaanse kruiden"], 
+            id: "j_w3_3", m: "Wo 21/01: Spaghetti 'Verstopte Groenten'", kcal: 600, p: 40, t: 45,
+            ing: ["VOORRAAD: 1kg Gehakt (Deel 1)", "Pasta", "Wortel/Prei/Courgette (geraspt)", "Tomatenpuree"], 
             steps: [
-                "1. Rul het gehakt in een grote pot tot het écht bruin is (dit geeft smaak).",
-                "2. Voeg fijngesneden ui, wortel en prei toe. Bak mee.",
-                "3. Voeg gezeefde tomaten/tomatenblokjes toe + **Italiaanse kruiden** en een blokje bouillon.",
-                "4. Laat minstens 20 min pruttelen. Kook de pasta."
+                "1. Rul het gehakt bruin.",
+                "2. Rasp wortel en courgette (of snij prei heel fijn) en bak mee. De groenten 'smelten' in de saus.",
+                "3. Voeg gezeefde tomaten toe. De extra groenten zorgen voor vezels die de suikerspiegel stabiel houden.",
+                "4. Serveer met pasta (liefst volkoren)."
             ], 
-            kid: "Sluurpspaghetti!" 
+            kid: "Rode saus (ze zien de groenten niet)." 
         },
         { 
-            id: "j_w3_4", m: "Do 22/01: Romige Kip Spinazie", kcal: 480, p: 32, t: 20,
-            ing: ["VOORRAAD: 350g Kipblokjes", "VOORRAAD: Rijst", "VOORRAAD: DV Spinazie", "Room", "Kerriepoeder"], 
+            id: "j_w3_4", m: "Do 22/01: Kip Spinazie & Kikkererwten", kcal: 490, p: 38, t: 20,
+            ing: ["VOORRAAD: 350g Kipblokjes", "VOORRAAD: DV Spinazie", "Kikkererwten (blik)", "Hüttenkäse (ipv room)"], 
             steps: [
-                "1. Kook de rijst.",
-                "2. Bak de kipblokjes met peper, zout en **kerriepoeder**.",
-                "3. Voeg de spinazie toe en laat ontdooien. Roer de room erdoor en warm goed door.",
-                "4. Proef en kruid bij met peper."
+                "1. Bak de kipblokjes met currykruiden.",
+                "2. Voeg de spinazie en de uitgelekte kikkererwten toe (extra eiwit/vezels!).",
+                "3. In plaats van vette room: roer er op het laatst 3 eetlepels Hüttenkäse of roomkaas light door.",
+                "4. Serveer met rijst."
             ], 
-            kid: "Rijst met groene roomsaus." 
+            kid: "Kip met balletjes (kikkererwten)." 
         },
         { 
-            id: "j_w3_5", m: "Vr 23/01: Kippenchipolata met Rode Kool", kcal: 450, p: 25, t: 20,
-            ing: ["Kippenchipolata's", "Pot Rode kool met appel", "Aardappelen"], 
+            id: "j_w3_5", m: "Vr 23/01: Chipolata met Broccolimix", kcal: 460, p: 32, t: 20,
+            ing: ["Kippenchipolata", "Broccoli", "Rode ui", "Zoete Aardappel"], 
             steps: [
-                "1. Schil en kook de aardappelen.",
-                "2. Bak de kippenworsten rustig in boter op laag vuur (niet prikken, dan blijven ze sappig).",
-                "3. Verwarm de rode kool.",
-                "4. Serveer klassiek met jus van de worstjes."
+                "1. Snij zoete aardappel en broccoli in roosjes/blokjes. Kook ze kort voor (5 min).",
+                "2. Bak de chipolata's in de pan.",
+                "3. Voeg de voorgekookte groenten toe aan de pan en roerbak alles samen in de vetstof van de worstjes.",
+                "4. Snel, één pan afwas en veel groen."
             ], 
-            kid: "Worstjes met zoete paarse groente." 
+            kid: "Worstjes met boompjes." 
         },
 
         /* WEEK 4 */
         { 
-            id: "j_w4_1", m: "Ma 26/01: Mexicaanse Chili Schotel", kcal: 495, p: 22, t: 25,
-            ing: ["Rode Bonen (blik)", "Mais (blik)", "VOORRAAD: Tomatenblokjes", "VOORRAAD: Rijst", "Komijn/Paprikapoeder"], 
+            id: "j_w4_1", m: "Ma 26/01: Chili Con Carne (Extra Bonen)", kcal: 510, p: 30, t: 30,
+            ing: ["Rode Bonen (2 blikken)", "Mais", "VOORRAAD: Tomatenblokjes", "VOORRAAD: Rijst", "Komijn"], 
             steps: [
-                "1. Fruit ui en knoflook. Voeg 1 tl **komijn** en 1 tl **paprikapoeder** toe.",
-                "2. Doe de tomatenblokjes, uitgelekte bonen en mais erbij.",
-                "3. Laat pruttelen tot een dikke saus. Serveer met rijst.",
-                "4. (Optie: Heb je nog een restje kaas of yoghurt? Lekker erbij!)"
+                "1. Fruit ui en knoflook met komijn en paprikapoeder.",
+                "2. Voeg tomatenblokjes toe. Gebruik VEEL bonen (zwarte bonen of kidneybonen). Bonen zijn super gezond.",
+                "3. (Optioneel: Restje gehakt kan erbij, maar hoeft niet).",
+                "4. Serveer met een lepel yoghurt (eiwit) in plaats van zure room."
             ], 
-            kid: "Cowboy rijst met maisjes." 
+            kid: "Chili met rijst." 
         },
         { 
-            id: "j_w4_2", m: "Di 27/01: Nasi Goreng (Kip & Prei)", kcal: 510, p: 28, t: 20,
-            ing: ["VOORRAAD: 300g Kipblokjes", "VOORRAAD: Rijst", "Prei", "2 Eieren", "Ketjap Manis"], 
+            id: "j_w4_2", m: "Di 27/01: Nasi met Omeletreepjes", kcal: 480, p: 30, t: 20,
+            ing: ["VOORRAAD: 300g Kipblokjes", "VOORRAAD: Rijst", "Prei", "3 Eieren", "Extra diepvrieserwten"], 
             steps: [
-                "1. Kook de rijst en laat afkoelen.",
-                "2. Bak de kipblokjes gaar. Voeg de preiringen toe.",
-                "3. Doe de rijst erbij en bak op hoog vuur. Breng op smaak met een flinke scheut **Ketjap**.",
-                "4. Bak apart 2 spiegeleitjes en leg die erop."
+                "1. Bak de kip. Voeg veel prei en een hand diepvrieserwten toe (erwten = extra proteïne).",
+                "2. Bak de rijst mee met ketjap.",
+                "3. Kluts de eieren, bak er een omelet van, snij in reepjes en meng door de rijst.",
+                "4. Door de ei, kip en erwten heb je een drievoudige eiwitbron."
             ], 
-            kid: "Gebakken rijst." 
+            kid: "Rijst met groene bolletjes." 
         },
         { 
-            id: "j_w4_3", m: "Wo 28/01: Spaghetti Bolognese (Deel 2)", kcal: 650, p: 35, t: 45,
-            ing: ["VOORRAAD: 1kg Gehakt (Restant)", "Pasta", "Wortel/Ui (restjes)", "Basilicum"], 
+            id: "j_w4_3", m: "Wo 28/01: Spaghetti (Deel 2 - Restje)", kcal: 600, p: 40, t: 15,
+            ing: ["Restant Saus", "Pasta", "Rauwe groentenknabbel erbij"], 
             steps: [
-                "1. Maak opnieuw een grote pot saus met de rest van het gehakt (of gebruik de ingevroren saus van vorige week).",
-                "2. Tip voor variatie: Voeg deze keer wat extra wortel toe voor een zoetere saus, of een scheutje melk voor romigheid.",
-                "3. Serveer met pasta en eventueel kaas."
+                "1. Warm de saus op.",
+                "2. Omdat spaghetti soms wat weinig 'bite' heeft qua groenten: zet een schaaltje rauwe paprika/komkommer op tafel tijdens het eten.",
+                "3. Serveer met de pasta."
             ], 
-            kid: "Spaghetti is altijd feest." 
+            kid: "Spaghetti tijd!" 
         },
         { 
-            id: "j_w4_4", m: "Do 29/01: Tuna Melt Pasta", kcal: 520, p: 30, t: 15,
-            ing: ["Pasta", "Tonijn (blik)", "Mais (restje)", "Room", "Geraspte kaas", "Dille (gedroogd)"], 
+            id: "j_w4_4", m: "Do 29/01: Tuna Pasta met Broccoli", kcal: 530, p: 35, t: 20,
+            ing: ["Pasta", "Tonijn (blik)", "Broccoli (stronk)", "Hüttenkäse/Roomkaas"], 
             steps: [
-                "1. Kook de pasta.",
-                "2. Verwarm de room met wat tomatenpuree en bouillon. Voeg tonijn en mais toe.",
-                "3. Meng met de pasta en roer er de kaas doorheen.",
-                "4. Kruid af met een beetje **dille** en peper."
+                "1. Kook de pasta. Gooi de laatste 4 minuten kleine broccoliroosjes bij het kokend water (bespaart pannen).",
+                "2. Giet af. Meng direct met de tonijn en hüttenkäse (smelt tot saus).",
+                "3. Kruid met zwarte peper en eventueel wat citroensap.",
+                "4. Super eiwitrijk door tonijn + zuivel + broccoli (bevat ook proteïne!)."
             ], 
-            kid: "Vispasta met kaassaus." 
+            kid: "Pasta met vis en zachte boompjes." 
         },
         { 
-            id: "j_w4_5", m: "Vr 30/01: Zoete Aardappel Curry (Vega)", kcal: 460, p: 18, t: 25,
-            ing: ["Zoete Aardappel", "Kikkererwten (blik)", "VOORRAAD: DV Spinazie", "Kokosmelk", "VOORRAAD: Rijst"], 
+            id: "j_w4_5", m: "Vr 30/01: Zoete Aardappel & Ei Curry", kcal: 450, p: 20, t: 25,
+            ing: ["Zoete Aardappel", "4 Eieren (gekookt)", "VOORRAAD: DV Spinazie", "Kokosmelk"], 
             steps: [
-                "1. Schil de zoete aardappel en snij in blokjes. Bak kort aan in olie met **kerriepoeder**.",
-                "2. Voeg de kokosmelk toe en laat sudderen tot de aardappel zacht is.",
-                "3. Doe de uitgelekte kikkererwten en diepvries spinazie erbij. Warm door.",
-                "4. Serveer met rijst. (Gezond, goedkoop en erg lekker!)"
+                "1. Maak de curry met zoete aardappel, spinazie en kokosmelk zoals eerder.",
+                "2. Kook intussen eieren hard.",
+                "3. Pel de eieren en doe ze in de currysaus (Egg Curry).",
+                "4. Goedkoop, vleesloos, maar toch veel proteïne en groenten."
             ], 
-            kid: "Zachte, zoete blokjes in gele saus." 
+            kid: "Gele saus met een eitje." 
         }
     ]
 };
